128751558ef5f3f82217111874daf574ac45f4ea
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-03-23 13:24:50 +0100

    viser kontiliste på forsiden

--- src/main/java/com/example/bank/HelloServlet.java
+++ src/main/java/com/example/bank/HelloServlet.java
@@ -1,6 +1,12 @@
 package com.example.bank;
 
+import Domæneobjekter.Konto;
+
 import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -8,13 +14,28 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
 
-    public void init() {
+    Map<String, Konto> kontoMap = new TreeMap<>();
 
+    public void init() {
+    Konto konto = new Konto("markus", "markus", 40);
+    Konto konto1 = new Konto("micki", "micki", 3000000);
+    Konto konto2 = new Konto("otto", "otto", 1);
+    Konto konto3 = new Konto("timi", "timi", 0);
+    Konto konto4 = new Konto("bo", "bo", 455);
+
+    kontoMap.put(konto.getNavn(), konto);
+    kontoMap.put(konto1.getNavn(), konto1);
+    kontoMap.put(konto2.getNavn(), konto2);
+    kontoMap.put(konto3.getNavn(), konto3);
+    kontoMap.put(konto4.getNavn(), konto4);
+
+        ServletContext servletContext = getServletContext();
+        servletContext.setAttribute("konti", kontoMap);
 
 
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 //        response.setContentType("text/html");
 //
 //        // Hello
@@ -22,6 +43,7 @@ public class HelloServlet extends HttpServlet {
 //        out.println("<html><body>");
 //        out.println("<h1>" + message + "</h1>");
 //        out.println("</body></html>");
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     public void destroy() {
